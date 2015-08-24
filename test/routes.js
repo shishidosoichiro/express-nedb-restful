619a0ebc98a6448aec701284368b8718c488700b
shishidosoichiro <shishidosoichiro@hotmail.com>
2015-08-24 18:38:01 +0900

    fix a test.

--- test/routes.js
+++ test/routes.js
@@ -2,43 +2,50 @@ var expect = require('chai').expect;
 var express = require('express');
 var Routes = require('../routes');
 
+var equal = function(expected){
+	return function(result){
+		expect(expected).to.be.equal(result)
+		return this;
+	}
+}
+var assign = function(src, dest){
+	for (var key in src) dest[key] = src[key];
+		return dest;
+}
+var Test = function(options){
+	var test = function(){
+		Routes(test).find(test, test);
+	};
+	assign(options, test);
+	test.find = function(params, callback){
+		expect(params).to.be.equal(this.params);
+		callback.apply(this, this.result)
+	};
+	return test;
+}
+
 describe('express-nedb-restful', function(){
 	describe('routes.js', function(){
 		describe('#find', function(){
-			var equal = function(expected){
-				return function(result){
-					expect(expected).to.be.equal(result)
-					return this;
-				}
-			}
-			var Test = function(options){
-				var test = function(){
-					Routes(test).find(test, test);
-				};
-				for (var key in options) test[key] = options[key];
-				test.find = function(params, callback){
-					expect(params).to.be.equal(this.params);
-					callback.apply(this, this.result)
-				};
-				return test;
-			}
 			it('should call "find" method of store.', function(){
 				var options = {
 					params: {a: 'b'},
 					result: [null, {c: 'd'}]
 				};
-				Test({
-					params: options.params,
-					result: options.result,
+				Test(assign(options, {
 					json: equal(options.result[1])
-				})();
+				}))();
 			});
-			it('should return 500 status when a store returns err object.', Test({
-				params: {},
-				result: [{message: 'error'}],
-				json: equal({message: 'error'}),
-				status: equal(500)
-			}));
+			it('should return 500 status when a store returns err object.', function(){
+				var options = {
+					params: {a: 'b'},
+					result: [{message: 'error'}]
+				};
+				Test(assign(options, {
+					json: equal(options.result[0]),
+					status: equal(500)
+				}))();
+			});
 		});
 	});
 });
